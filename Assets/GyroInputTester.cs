4ed0b7a356af42c75b70fd467c6579f6a8ae8bcb
Keijiro Takahashi <keijiro@gmail.com>
2022-10-11 12:32:25 +0900

    Refactoring

--- Assets/GyroInputTester.cs
+++ Assets/GyroInputTester.cs
@@ -5,57 +5,77 @@ using UnityEngine.InputSystem.Layouts;
 
 sealed class GyroInputTester : MonoBehaviour
 {
+    #region Static class members
+
+    // Layout extension written in JSON
+    const string LayoutJson = @"{
+      ""name"": ""DualShock4GamepadHIDCustom"",
+      ""extend"": ""DualShock4GamepadHID"",
+      ""controls"": [
+        {""name"":""gyro"", ""format"":""VC3S"", ""offset"":13,
+         ""layout"":""Vector3"", ""processors"":""ScaleVector3(x=-1,y=-1,z=1)""},
+        {""name"":""gyro/x"", ""format"":""SHRT"", ""offset"":0 },
+        {""name"":""gyro/y"", ""format"":""SHRT"", ""offset"":2 },
+        {""name"":""gyro/z"", ""format"":""SHRT"", ""offset"":4 },
+        {""name"":""accel"", ""format"":""VC3S"", ""offset"":19,
+         ""layout"":""Vector3"", ""processors"":""ScaleVector3(x=-1,y=-1,z=1)""},
+        {""name"":""accel/x"", ""format"":""SHRT"", ""offset"":0 },
+        {""name"":""accel/y"", ""format"":""SHRT"", ""offset"":2 },
+        {""name"":""accel/z"", ""format"":""SHRT"", ""offset"":4 }
+      ]}";
+
+    #endregion
+
+    #region Private members
+
+    // Accumulation of gyro input
     Quaternion _accGyro = Quaternion.identity;
 
+    // Accelerometer input control
+    Vector3Control _accel;
+
+    // Gyro vector data to rotation conversion
     Quaternion GyroInputToRotation(InputAction.CallbackContext ctx)
       => Quaternion.Euler(ctx.ReadValue<Vector3>() * Mathf.PI * 2);
+
+    #endregion
+
+    #region MonoBehaviour implementation
 
     void Start()
     {
         // DS4 input layout extension
-        InputSystem.RegisterLayoutOverride(@"
-{""name"": ""DualShock4GamepadHIDCustom"",
- ""extend"": ""DualShock4GamepadHID"",
- ""controls"": [
-   { ""name"": ""gyro"",    ""format"": ""VC3S"", ""offset"": 13, ""layout"": ""Vector3"" },
-   { ""name"": ""gyro/x"",  ""format"": ""SHRT"", ""offset"": 0 },
-   { ""name"": ""gyro/y"",  ""format"": ""SHRT"", ""offset"": 2 },
-   { ""name"": ""gyro/z"",  ""format"": ""SHRT"", ""offset"": 4 },
-   { ""name"": ""accel"",   ""format"": ""VC3S"", ""offset"": 19, ""layout"": ""Vector3"" },
-   { ""name"": ""accel/x"", ""format"": ""SHRT"", ""offset"": 0 },
-   { ""name"": ""accel/y"", ""format"": ""SHRT"", ""offset"": 2 },
-   { ""name"": ""accel/z"", ""format"": ""SHRT"", ""offset"": 4 }
- ]}"
-        );
+        InputSystem.RegisterLayoutOverride(LayoutJson);
 
         // Gyroscope input callback
         var action = new InputAction(binding: "<Gamepad>/gyro");
         action.performed += ctx => _accGyro *= GyroInputToRotation(ctx);
         action.Enable();
+
+        // Accelerometer input control
+        _accel = Gamepad.current.GetChildControl<Vector3Control>("accel");
     }
 
     void Update()
     {
         // Current status
         var rot = transform.localRotation;
-        var gyro = _accGyro;
+
+        // Rotation from gyroscope
+        rot *= _accGyro;
         _accGyro = Quaternion.identity;
 
-        // Delta rotation from gyroscope
-        rot *= new Quaternion(-gyro.x, -gyro.y, gyro.z, gyro.w);
-
-        // Gravity vector from accelerometer
-        var accel = Gamepad.current.GetChildControl<Vector3Control>("accel");
-        var gravity = Vector3.Scale(accel.ReadValue(), new Vector3(-1, -1, 1));
-
-        // Compensation
+        // Drift compensation using gravitational acceleration
+        var gravity = _accel.ReadValue();
         var comp = Quaternion.FromToRotation(rot * gravity, -Vector3.up);
 
-        // Compensation weakening
+        // Compensation reduction
         comp.w *= 10.0f;
         comp = comp.normalized;
 
         // Update
         transform.localRotation = comp * rot;
     }
+
+    #endregion
 }
